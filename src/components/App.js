4f715d54d9cd413bc1ece5647b6ee34ea1a7e389
Swati jagtap <swatijagtap5060@gmail.com>
2024-03-01 11:48:29 +0530

    Submitted

--- src/components/App.js
+++ src/components/App.js
@@ -5,26 +5,29 @@ import TodoList from "./TodoList";
 import { useState } from "react";
 
 const App = () => {
-  const [todos, setTodos] = useState([
+  let todo=[
     { id: 1, text: 'Learn React', completed: false },
     { id: 2, text: 'Build a React app', completed: false },
     { id: 3, text: 'Deploy React app', completed: false }
-  ]);
+  ]
+  const [todos, setTodos] = useState(todo);
+
+  function buttonChange(id){
+     console.log(id,"parent")
+     setTodos(todos.filter((item)=>id!==item.id))
+  }
 
 
-  const handleComplete = (id) => {
-    const updatedTodos = todos.map(todo =>
-      todo.id === id ? { ...todo, completed: true } : todo
-    );
-    setTodos(updatedTodos);
-  };
   
 
   return (
     <div>
       <h1>Todo List</h1>
-      {/* Render TodoList component and pass todos and handleComplete function */}
-      <TodoList todos={todos} handleComplete={handleComplete} />
+      {/* Render TodoList component and pass to dos and handleComplete function */}
+      {todos.map((todo)=><TodoList  key={todo.key} id={todo.id} text={todo.text}
+       buttonChange={buttonChange}completeState={todo.completed} />)
+        }
+      
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,20 +1,19 @@
-import React from "react";
-function TodoList({ todos, handleComplete }) {
+import React, { useState } from "react";
+
+
+function TodoList(todos, buttonChange) {
+    const [btn,setbtn]=useState("")
+    function handleChange(id){
+         
+       console.log(id)
+      todos. buttonChange(id)
+        
+    }
     return (
+        
       <ul>
-        {/* Map through todos and render each todo item */}
-        {todos.map(todo => (
-          <li key={todo.id}>
-            {/* Display todo text */}
-            {todo.text}
-            {/* Render Complete button for incomplete todos */}
-          {
-            !todo.completed && <button onClick={() => handleComplete(todo.id)}>Complete</button>
-          }
-              
-           
-          </li>
-        ))}
+        <li>{todos.text}</li>
+        <button onClick={(e)=>handleChange(todos.id)}>click</button>
       </ul>
     );
   }
